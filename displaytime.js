04134bc5e81dca88e1393fe1cfe3c0a06a330b2e
LI, PING <patriclouis.lee@gmail.com>
2013-10-03 19:44:35 +0800

    Update displaytime.js

--- displaytime.js
+++ displaytime.js
@@ -1,11 +1,19 @@
 // Display time & date
-// Created on by patriclouis @ ZJU on Oct. 3, 2013.
+// Created by patriclouis @ ZJU on Oct. 3, 2013.
 
+// Define variables
 var d = new Date();
 var date; 
 var week; 
+var day   = d.getDay();
+var month = d.getMonth();
+var year  = d.getFullYear();
+var hour  = d.getHours();
+var minute= d.getMinutes();
+var second= d.getSeconds();
 
-switch(d.getDay())
+// Identify the right day mark
+switch(day)
 {
 	case 0:
 	week = "Sunday"
@@ -45,13 +53,7 @@ if(d.getDay()==5)
 if(d.getDay()==6) 
 	week="Saturday" */
 
-var day   = d.getDay();
-var month = d.getMonth();
-var year  = d.getFullYear();
-var hour  = d.getHours();
-var minute= d.getMinutes();
-var second= d.getSeconds();
-
+// Show greetings
 if(hour>0 && hour<=12) 
 	msg = "Good morning!"
 if(hour>12 && hour<=18)
@@ -60,12 +62,16 @@ if(hour>18 && hour<=24)
 	msg = "Good evening!"
 if(hour>22 || hour<6)
     msg = "Hey, it is time to sleep and do not be too late!"
+    
+// Add "0" when the digit is smaller than 10
 if(second<10)
 	second="0"+second
 if(minute<10)
 	minute="0"+minute
 
-date = "<br>&nbsp;<br><b>"+ msg +"</b>"+"&nbsp; Now is: "+(d.getYear())+"-"+(d.getMonth()+1)+"-"+(d.getDay()-1)+" "; 
+// Concatenate the elements of date
+date = "<br>&nbsp;<br><b>"+ msg +"</b>"+"&nbsp; Now is: "+ year + "-"+ (month+1)+"-"+(day-1)+" "; 
 time = hour + ":" + minute + ":" + second; 
 
+// Write the ultimate time & date on HTML
 document.write("<span>"+ date + week +" "+ time +"</span><br>&nbsp;<br>");
